bd9d43a5cee02b8321a04c00755007d0b9502d7f
Parmjeet <iamparmjeetmishra@gmail.com>
2025-02-23 22:57:27 +0530

    GED

--- 06-JS-Fast/W0602/6.2-index.js
+++ 06-JS-Fast/W0602/6.2-index.js
@@ -1,3 +1,13 @@
-console.log('1st')
+// GED
 
-setTimeout
+console.log("age is", age)
+
+var age = 22 // undefined 
+// let age = 22 // JS knows but cannot let us access -read err again
+// Cannnot access 'age' before initialization
+
+// test()
+
+// var test = function () {
+//   console.log("testing")
+// }
