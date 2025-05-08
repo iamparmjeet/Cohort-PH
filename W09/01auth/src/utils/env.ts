fb92e812c332fffd6f000a809b9a6eaa28662300
parm <iamparmjeetmishra@gmail.com>
2025-05-08 21:07:37 +0530

    jwt secret added in env

--- W09/01auth/src/utils/env.ts
+++ W09/01auth/src/utils/env.ts
@@ -17,6 +17,7 @@ const EnvSchema = z.object({
   PORT: z.coerce.number().default(9999),
   LOG_LEVEL: z.enum(["fatal", "error", "warn", "info", "debug", "trace", "silent"]),
   SITE_URL: z.string().url(),
+  JWT_SECRET:z.string(),
   MAILTRAP_HOST: z.string(),
   MAILTRAP_PORT: z.coerce.number(),
   MAILTRAP_AUTH_USER: z.string(),
