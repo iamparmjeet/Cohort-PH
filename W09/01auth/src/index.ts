2721d8b0b7580b52ad95151419cb2e3631574eee
parm <iamparmjeetmishra@gmail.com>
2025-05-08 14:05:20 +0530

    Updated with user routes

--- W09/01auth/src/index.ts
+++ W09/01auth/src/index.ts
@@ -3,25 +3,53 @@ import express from "express";
 
 import * as middlewares from "@/middlewares/middlewares";
 import { pinoLogger } from "@/middlewares/pino-logger";
+import userRoutes from "@/routes/user/user.routes";
 
+import DB from "./utils/db";
 import env from "./utils/env";
 
 const app = express();
 
 const port = env.PORT || 8000;
 
+app.use(pinoLogger());
+
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
+
+app.use(cors({
+  origin: env.SITE_URL,
+  credentials: true,
+  methods: ["GET", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
+
 app.get("/health", (req, res) => {
-  res.send("Health Ok");
+  res.json({
+    message: "Health Ok",
+  });
+});
+
+// connect db
+(async () => {
+  await DB();
+})();
+
+// routes
+const routes = [
+  userRoutes,
+] as const;
+
+routes.forEach((route) => {
+  app.use("/api/v1/", route);
 });
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log(`Listening on port ${port}...`);
 });
-
-app.use(cors());
-app.use(express.json());
-app.use(pinoLogger());
 
 app.use(middlewares.serveEmojiFavicon("✨"));
 
