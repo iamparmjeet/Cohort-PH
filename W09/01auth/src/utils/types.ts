125bd6473241dbe653824940896068b11db3b6b5
parm <iamparmjeetmishra@gmail.com>
2025-05-08 14:04:12 +0530

    user types added

---
+++ W09/01auth/src/utils/types.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const userZodSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6), // you can tweak min length as needed
+  role: z.enum(["user", "admin"]).default("user"),
+  isVerified: z.boolean().default(false),
+  verificationToken: z.string().optional(),
+  resetPasswordToken: z.string().optional(),
+  resetPasswordExpires: z.date().optional(),
+  // timestamps added by Mongoose
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+});
+
+export const userInputSchema = userZodSchema.omit({
+  role: true,
+  isVerified: true,
+  verificationToken: true,
+  resetPasswordExpires: true,
+  resetPasswordToken: true,
+  createdAt: true,
+  updatedAt: true,
+});
+
+export type UserType = z.infer<typeof userZodSchema>;
+export type UserInputType = z.infer<typeof userInputSchema>;
