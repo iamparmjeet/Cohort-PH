c16215d4ecce3d3b2d4a07b6692ecfd919d8cb73
parm <iamparmjeetmishra@gmail.com>
2025-05-08 21:10:33 +0530

    register function updated with safeparse value and login controller created

--- W09/01auth/src/routes/user/user.controller.ts
+++ W09/01auth/src/routes/user/user.controller.ts
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 import type { Request, Response } from "express";
-
+import jwt from "jsonwebtoken"
 import crypto from "node:crypto";
 
 import User from "@/db/user.model";
 import env from "@/utils/env";
-import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR, OK } from "@/utils/http-status-codes";
-import { transport } from "@/utils/lib";
+import { BAD_REQUEST, CREATED, INTERNAL_SERVER_ERROR, METHOD_NOT_ALLOWED, OK } from "@/utils/http-status-codes";
+import { generateToken, sendEmailWithVerificationToken, transport } from "@/utils/lib";
+import { userInputSchema, userLoginSchema } from "@/utils/types";
+import bcrypt from "bcryptjs";
 
 
 
@@ -21,14 +23,18 @@ export async function registerUser(req: Request, res: Response) {
   // send success status to user
 
   // get data
-  const { name, email, password } = req.body || {};
+  const parsedData = userInputSchema.safeParse(req.body)
+
   // validate
-  if (!name || !email || !password) {
+  if (!parsedData.success) {
     res.status(BAD_REQUEST).json({
-      message: BAD_REQUEST,
+      message: "Input data invalid",
+      errors: parsedData.error.flatten()
     });
     return
   }
+
+  const {name, email, password} = parsedData.data
 
   // find in DB
   try {
@@ -36,7 +42,7 @@ export async function registerUser(req: Request, res: Response) {
 
     if (existingUser) {
       res.status(BAD_REQUEST).json({
-        message: "User already exists",
+        message: "User already exists, Please login",
       });
       return
     }
@@ -53,31 +59,7 @@ export async function registerUser(req: Request, res: Response) {
 
     console.log("user", user);
 
-    // token
-    const token = crypto.randomBytes(32).toString("hex");
-    console.log("token", token);
-
-    // save the token in db
-    user.verificationToken = token;
-    await user.save();
-
-    const verificationLink = `${env.SITE_URL}/api/v1/users/verify/${token}`
-    // email
-    const mailOptions = await transport.sendMail({
-        from: "Parm <parm@example.com>",
-        to: user.email || "",
-        subject: "Verify your email",
-        text: `Please click on the following link to verify:
-          ${verificationLink}
-        `,
-        html: `<p>Hello ${user.name},</p>
-            <p>Please verify your email by clicking the link below:</p>
-            <a href="${verificationLink}">${verificationLink}</a>`,
-      });
-
-    console.log("Message sent:", mailOptions);
-    const sendMail = await transport.sendMail(mailOptions)
-    console.log("Sendmail", sendMail)
+    await sendEmailWithVerificationToken(user)
     
     res.status(CREATED).json({
       message: "User Register successfully. Please verify your email."
@@ -142,4 +124,105 @@ export async function verifyUser(req: Request, res: Response) {
   }
 
 
+}
+
+export async function login(req: Request, res: Response) {
+  // get data from body
+  // verify body data
+  // check if the email exits in db
+  // if not return error
+  // if yes - check whether password is matched with hashedpassword in db
+  // if not return error
+  // if yes gives back jwt token expiry
+
+  // get data from body and verify
+  const parsedData = userLoginSchema.safeParse(req.body)
+
+  if (!parsedData.success) {
+    res.status(BAD_REQUEST).json({
+      message: "Validation Failed",
+      errors: parsedData.error.flatten()
+    })
+    return
+  }
+
+  const {email, password} = parsedData.data
+  
+  try {
+
+    const user = await User.findOne({ email })
+    
+    if (!user) {
+      res.status(BAD_REQUEST).json({
+        message: "Invalid Creditionals"
+      })
+      return
+    }
+    console.log("user", user)
+
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      user.password
+    )
+
+    if (!isPasswordMatch) {
+      res.status(BAD_REQUEST).json({
+        message: "Invalid Creditionals"
+      })
+      return
+    }
+
+    // check if user is verified or not
+    const userVerifiedStatus = user.isVerified
+
+    if (!userVerifiedStatus) {
+      await sendEmailWithVerificationToken(user)
+      res.status(METHOD_NOT_ALLOWED).json({
+        message: "Please verify before login. Verfication Link Sent"
+      })
+      return
+    }
+
+    // TODO: sending email again for verification
+
+    // token generate
+    const token = jwt.sign(
+      {
+      id: user._id,
+      role: user.role,
+      name: user.name,
+      email: user.email
+      },
+      env.JWT_SECRET,
+      {
+      expiresIn: "24h"
+      }
+    )
+
+    console.log("token", token)
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      maxAge: 25*60*600*1000 // 24 hr
+    })
+
+    res.status(OK).json({
+      message: "Login successfully",
+      success: true,
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email
+      }
+    })
+    return
+
+  } catch (error) {
+    console.error(error)
+    res.status(INTERNAL_SERVER_ERROR).json({
+      message: "Unable to login"
+    })
+  }
 }
