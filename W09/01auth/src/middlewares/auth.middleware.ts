929cd06044aa789422e54bd6c2fda4de5aede7be
parm <iamparmjeetmishra@gmail.com>
2025-05-23 15:42:20 +0530

    auth middleware created

--- W09/01auth/src/middlewares/auth.middleware.ts
+++ W09/01auth/src/middlewares/auth.middleware.ts
@@ -1,5 +1,9 @@
-import { INTERNAL_SERVER_ERROR } from "@/utils/http-status-codes"
+import jwt from "jsonwebtoken"
+
+import { FORBIDDEN, INTERNAL_SERVER_ERROR } from "@/utils/http-status-codes"
 import type { NextFunction, Request, Response } from "express"
+import env from "@/utils/env"
+import type { JwtUserPayload } from "@/utils/types"
 
 export const isLoggedIn = async function (req:Request, res:Response, next: NextFunction) {
   // get the token from the cookie
@@ -9,14 +13,45 @@ export const isLoggedIn = async function (req:Request, res:Response, next: NextF
   // if match give user profile
   
   try {
-    const cookie = req.cookies.token
-    console.log("cookie", cookie)
-  
+    const token = req.cookies.token
+
+    console.log("token1", token)
+    
+    if (!token) {
+      res.status(FORBIDDEN).json({
+        message: "Authenitication token missing",
+        success: false
+      })
+      return
+    }
+
+    // extract the token details
+    const docodedUser = jwt.verify(token, env.JWT_SECRET) as JwtUserPayload
+    console.log("isDocoded", docodedUser)
+
+    if (!docodedUser) {
+      res.clearCookie("token", {
+        httpOnly: true,
+        secure: env.NODE_ENV === "production",
+        sameSite: "strict"
+      })
+    res.status(FORBIDDEN).json({
+        message: "Unauthorized access",
+        success: false
+    })
+      return
+    }
+
+    req.user = docodedUser
+    console.log("userfromMiddleware", req.user)
+    
+    next()
   } catch (error) {
     console.error(error)
-      res.status(INTERNAL_SERVER_ERROR).json({
-          message: "Unauthorized access"
-        })
+    res.status(INTERNAL_SERVER_ERROR).json({
+      message: "Unauthorized access",
+      success: false
+    })
+    return
   }
-  next()
 }
