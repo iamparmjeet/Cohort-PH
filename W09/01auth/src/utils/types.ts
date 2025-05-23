96d09d105194ad57bb8b4e03ecf3e1e61624f394
parm <iamparmjeetmishra@gmail.com>
2025-05-23 15:15:18 +0530

    user enum added and exported

--- W09/01auth/src/utils/types.ts
+++ W09/01auth/src/utils/types.ts
@@ -1,10 +1,16 @@
+import { Types } from "mongoose";
 import { z } from "zod";
+
+export enum UserRole {
+  "user" = "user",
+  "admin" = "admin",
+}
 
 export const userZodSchema = z.object({
   name: z.string(),
   email: z.string().email(),
   password: z.string().min(6), // you can tweak min length as needed
-  role: z.enum(["user", "admin"]).default("user"),
+  role: z.nativeEnum(UserRole).default(UserRole.user),
   isVerified: z.boolean().default(false),
   verificationToken: z.string().optional(),
   resetPasswordToken: z.string().optional(),
@@ -31,3 +37,14 @@ export const userLoginSchema = userInputSchema.omit({
 export type UserType = z.infer<typeof userZodSchema>;
 export type UserInputType = z.infer<typeof userInputSchema>;
 export type UserLoginType = z.infer<typeof userLoginSchema>
+
+// ***************** Other Types ******************
+
+export type JwtUserPayload = {
+  id: string | Types.ObjectId;
+  role: UserRole;
+  name: string;
+  email: string;
+  iat?: number;
+  exp?: number
+}
