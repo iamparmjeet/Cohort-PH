84a6fb57c85b6f2d0c91f92070a605070666acf8
iamparmjeet <iamparmjeetmishra@gmail.com>
2026-02-16 23:23:55 +0530

    Update=>Ques folder added to practive given question

---
+++ Ques/14Loop.js
@@ -0,0 +1,17 @@
+// You have a pile of gifts, and you want to give each of your friends one gift at a time. You keep giving them gifts until they all get one.
+//
+// Write a function to distribute gifts to your friends one by one. It should stop once all your friends have received a gift.
+//
+
+function distributeGifts(totalGifts, friends) {
+	let giftsGiven = 0;
+	for (let i = 0; i < friends; i++) {
+		if (totalGifts > 0) {
+			giftsGiven++;
+			totalGifts--;
+		}
+	}
+	return giftsGiven;
+}
+
+console.log(distributeGifts(11, 5));
